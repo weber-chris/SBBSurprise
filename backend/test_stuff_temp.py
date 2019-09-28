27f3beba39be1a18ef19ff3cb641b07199e54eb2
Christoph Weber <christoph.t.weber@gmail.com>
2019-09-28 12:49:57 +0200

    temporary file to try stuff

---
+++ backend/test_stuff_temp.py
@@ -0,0 +1,9 @@
+def __call_SBB_api(suitable_destinations):
+    """
+    Call SBB Api to get offer for activity-based filtered cities within time & duration boundaries
+    """
+    # TODO call api for each destination
+    print('bla')
+    return []
+
+__call_SBB_api(None)
