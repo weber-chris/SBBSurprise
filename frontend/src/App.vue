04ee26a5ec4fe52d51680873a9732a8cf39f8a9f
Alexandre Moeri <alex.moeri@gmail.com>
2019-09-28 20:56:42 +0200

    fe collects user preferences

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,17 +6,14 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      user: {}
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
