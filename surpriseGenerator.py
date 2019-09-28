8be93f518730824ef7f925826dbfc65160a143c1
Christoph Weber <christoph.t.weber@gmail.com>
2019-09-28 07:03:39 +0200

    skeleton & basic workflow for surprise generation

--- surpriseGenerator.py
+++ surpriseGenerator.py
@@ -1,55 +1,94 @@
 class Suprise:
-    def __init__(self):
-        pass
-
-    def get_suitable_offers(self, date, budget, max_duration, location, preferences, turned_down_offers):
+    def __init__(self, date, budget,  start_location, preferences, people):
         """
-        Get requested surprise constraints, return list of possible destinations with respective possible activities,
-        ordered by suitability  score
-        possible acitivi
         :param date: date when surprise should take place
         :param budget: low/mid/high/None
         :param max_duration: how long can the trip be (i.e. max_radius) in minutes
-        :param location: starting city
+        :param start_location: starting city
         :param preferences: dictionary for activity types with score
-        :param turned_down_offers: list of destinations to exclude (because of reroll)
-        :return:
-            {city_1 :
-                {score:int,
-                trip_data:
-                    {price:double,
-                    start_time:DateTime,
-                    hints:[..]}
-                activities:
-                    {activity_1:score, activity_2:score, ...}
-                },
-            city_2: ...}
+        :param people: how many people will participate
         """
-        suitable_cities = self.__get_suitable_cities(preferences, turned_down_offers)
+        self.date = date
+        self.budget = budget
+        self.max_duration = 180 # max time for one way in train
+        self.start_location = start_location
+        self.preferences = preferences
+        self.people = people
 
+    def get_offers(self):
+        """
+        Gets list of possible destinations with respective possible activities and suitability score
+        """
+        activities = self.__get_all_activities()
+        destinations = self.__get_all_destinations()
+        suitable_offers = self.__get_suitable_offers(activities, destinations)
+        scored_offers = self.__score_suitable_offers(suitable_offers)
+        return scored_offers
 
-    def __get_activities(self):
+    def __get_all_activities(self):
         """
-        Get all possible activities per city
-        :return: [city_1: [{activity_1: [labels_1, label_2..],{activity_2:..},
-                  city_2: ..]
+        Get all activities with their labels and cities
         """
+        # TODO read from DB/CSV
         return []
 
-    def __get_suitable_cities(self, preferences, turned_down_offers):
+    def __get_all_destinations(self):
         """
-        Get all cities that match offers, rated with a suitability score
-        :param preferences: dictionary for activity types with score
-        :param turned_down_offers: list of destinations to exclude (because of reroll)
-        :return: [city_1: {score:double, [{activity_1: [labels_1, label_2..],{activity_2:..}},
-                  city_2: ..]
+        Get all destinations with their hints
         """
+        # TODO read from DB/CSV
         return []
 
-    def __suitable_offers_from_api(self, suitable_cities):
+    def __get_suitable_offers(self, activities, destinations):
         """
-        Get offer for suitable cities within time & duration boundaries
-        :param suitable_cities:
-        :return:
+        Get all cities that match offers
         """
-        pass
+        # TODO filter activities based on preferences
+        suitable_destinations = self.__get_suitable_destinations(activities, destinations)
+        # TODO call SBB API for all destinations
+        offers = self.__call_SBB_api(suitable_destinations)
+        # TODO filter offers based on duration and cost
+        suitable_offers = self.__filter_offers(offers)
+        return []
+
+    def __get_suitable_destinations(self, activities, destinations):
+        """
+        Get all destinations that match preferences
+        """
+        # TODO merge activities & cities
+        # should we already kick out some destinations here?
+        return []
+
+    def __call_SBB_api(self, suitable_destinations):
+        """
+        Call SBB Api to get offer for activity-based filtered cities within time & duration boundaries
+        """
+        # TODO call api for each destination
+        return []
+
+    def __filter_offers(self, offers):
+        """
+        Drop all offers that are against price/duration constraint
+        """
+        # TODO drop unsuitable offers
+        return []
+
+    def __score_suitable_offers(self, suitable_offers):
+        """
+        Give a suitability score, weighted based on activity-preferences and price
+        """
+        # TODO calculate weighted score that predicts, how much a user "likes" the destination
+        return []
+
+
+class Destination:
+    name = None
+    hints = None
+    activities = None
+    price = -1
+    score = -1
+    start_time = None
+
+    def __init__(self, name, hints):
+        self.name = name
+        self.hints = hints
